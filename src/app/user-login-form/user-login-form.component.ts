37f6fa2f0bf98f12a93bff686831f05da5aff532
jdussold <jdussold@gmail.com>
2023-05-09 16:19:50 -0500

    Updated login form extensively

--- src/app/user-login-form/user-login-form.component.ts
+++ src/app/user-login-form/user-login-form.component.ts
@@ -1,8 +1,9 @@
-// Importing necessary components and services from Angular Material and our custom FetchApiDataService
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { FetchApiDataService } from '../fetch-api-data.service';
 import { MatDialogRef } from '@angular/material/dialog';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-user-login-form',
@@ -11,41 +12,57 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 })
 export class UserLoginFormComponent implements OnInit {
   // Declare the userData object with the Username and Password properties
-  @Input() userData: {
-    Username: string;
-    Password: string;
-  } = { Username: '', Password: '' };
+  userData = { Username: '', Password: '' };
+
+  // Add a flag to check if there's an error during login
+  loginError: boolean = false;
+  loginErrorMessage: string = '';
+  loading: boolean = false;
+
+  // Define the login form
+  loginForm: FormGroup = this.fb.group({
+    Username: ['', Validators.required],
+    Password: ['', Validators.required],
+  });
 
   constructor(
-    // Inject the FetchApiDataService, MatDialogRef, and MatSnackBar services
+    private fb: FormBuilder,
     public fetchApiData: FetchApiDataService,
     public dialogRef: MatDialogRef<UserLoginFormComponent>,
-    public snackBar: MatSnackBar
+    public snackBar: MatSnackBar,
+    public router: Router
   ) {}
 
   ngOnInit(): void {}
 
   loginUser(): void {
-    // Destructure the userData object to get the values of Username and Password
-    const { Username, Password } = this.userData;
-    // Call the userLogin method from the FetchApiDataService service, passing in the user's login information
-    this.fetchApiData.userLogin({ Username, Password }).subscribe(
-      // Handle the successful response from the server
-      (result: any) => {
-        // Save the current user's information and token to localStorage
-        localStorage.setItem('user', result.user.Username);
+    const { Username, Password } = this.loginForm.value;
+    this.loading = true;
+    this.fetchApiData.userLogin({ Username, Password }).subscribe({
+      next: (result: any) => {
+        this.loading = false;
+        this.loginError = false;
+        localStorage.setItem('user', JSON.stringify(result.user));
         localStorage.setItem('token', result.token);
-        // Close the dialog window
         this.dialogRef.close();
-        // Display a success message using the MatSnackBar service
         this.snackBar.open('Logged in successfully!', 'OK', { duration: 2000 });
+        this.router.navigate(['movies']);
       },
-      // Handle the error response from the server
-      (error: any) => {
-        // Display the error message (or a default message if none is provided) using the MatSnackBar service
-        const errorMessage = error.error.message || 'Unknown error occurred!';
-        this.snackBar.open(errorMessage, 'OK', { duration: 2000 });
-      }
-    );
+      error: (error: any) => {
+        this.loading = false;
+        this.loginError = true;
+        if (error.status === 400) {
+          this.loginErrorMessage =
+            error.error.message ||
+            'Invalid username or password. Please try again.';
+        } else {
+          const errorMessage = error.error.message || 'Unknown error occurred!';
+          this.loginErrorMessage = errorMessage;
+        }
+      },
+      complete: () => {
+        console.log('Observable completed');
+      },
+    });
   }
 }
