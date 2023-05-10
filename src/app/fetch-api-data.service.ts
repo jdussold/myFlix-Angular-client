ea2a25da04e00b16f23a6a224a94c46134ab6f54
jdussold <jdussold@gmail.com>
2023-05-09 22:16:55 -0500

    refactored depreciated code

--- src/app/fetch-api-data.service.ts
+++ src/app/fetch-api-data.service.ts
@@ -247,6 +247,8 @@ export class FetchApiDataService {
         `Error Status code ${error.status},` + `Error body is: ${error.error}`
       );
     }
-    return throwError('Something bad happened; please try again later');
+    return throwError(
+      () => new Error('Something bad happened; please try again later')
+    );
   }
 }
